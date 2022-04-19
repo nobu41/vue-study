844842340b33b1e4db3a4726c7099b8dd2266895
nobu41 <ebinobu.project@gmail.com>
2022-04-19 10:40:06 +0900

    初回コミット レッスンファイルのアップロード

---
+++ lesson/2-03/main.js
@@ -0,0 +1,9 @@
+new Vue({
+  el: '#app',
+  data:{
+    isChild: true,
+    isActive: true,
+    textColor: '#f00',
+    bgColor: 'lightgray'
+  }
+})
---
+++ lesson/2-05/main.js
@@ -0,0 +1,10 @@
+new Vue ({
+  el:'#app',
+  data:{
+    list:[
+      {id:1, name:'ピカ様',hp:100},
+      {id:2, name:'イワーク', hp:301},
+      {id:3, name:'タケシ', hp:500}
+    ]
+  }
+})
