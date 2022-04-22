07657300f6a7b1804ba5b9def604d2ae60207f07
nobu41 <ebinobu.project@gmail.com>
2022-04-22 17:24:31 +0900

    lesson3-1～6

---
+++ lesson/3/3-03/main.js
@@ -0,0 +1,14 @@
+//【参考】stop
+//https://codepen.io/shohei-japan/pen/BMwgzW?editors=1010
+//【参考】prevent
+//https://codepen.io/shohei-japan/pen/WPZWgq
+
+//イベント修飾子
+new Vue({
+  el: '#app',
+  methods: {
+    handler: function (comment) {
+      console.log(comment)
+    }
+  }
+})
---
+++ lesson/3/3-04/main.js
@@ -0,0 +1,8 @@
+//S14 フォーム入力バインディング
+//複数行テキスト
+new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello!'
+  }
+})
