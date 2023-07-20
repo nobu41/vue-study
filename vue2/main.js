6af8addbd3582123c80b7651ac68eec6cbf605f0
user <>
2023-07-20 09:53:42 +0900

    【veu2】トランジション＆アニメーション

--- vue2/main.js
+++ vue2/main.js
@@ -1,14 +1,6 @@
 var app = new Vue({
     el: '#app',
     data: {
-        message:'初期メッセージ',
-        messageNumber:'数値としてうけとる',
-        list: ['りんご','バナナ','いちご'],
-        show: false
-    },
-    methods: {
-        handleClick: function(event) {
-            alert(event.target)//[object HTMLButtonElement]
-        }
+        show: true
     }
   })
