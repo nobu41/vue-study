f338ce5036d1e5a9920dc430c2118b0ac55f4cf2
user <>
2023-07-24 21:43:41 +0900

    【vue2】リアクティブなデータの定義

--- vue2/main.js
+++ vue2/main.js
@@ -1,6 +1,20 @@
+//オプションの外に定義されたデータでもVue.jsとして登録するとリアクティブデータに変換される
+var state = { count: 0 }
+
 var app = new Vue({
     el: '#app',
     data: {
-        show: true
+        state: state,
+        message: 'Veu.js', //messageは変化を検知できるようになる
+        //dataオプションの直下プロパティは後から追加できないため、
+        //内容が決まってなくても空のデータとして定義しておく(なるべく後から代入されるデータと同じ型で定義する)
+        //例
+        newTodoText: '',
+        visitcount: 0,
+        hideCompletedToDos: false,
+        todos: [],
+        error: null
     }
   })
+
+state.count++ // state.countはリアクティブデータになっている
