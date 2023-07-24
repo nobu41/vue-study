ba9927266f57cdcd3bebee193b69ce85d807f37a
user <>
2023-07-24 22:02:35 +0900

    【vue2】テキストのデータバインディング

--- vue2/main.js
+++ vue2/main.js
@@ -1,20 +1,14 @@
-//オプションの外に定義されたデータでもVue.jsとして登録するとリアクティブデータに変換される
-var state = { count: 0 }
-
 var app = new Vue({
     el: '#app',
     data: {
-        state: state,
-        message: 'Veu.js', //messageは変化を検知できるようになる
-        //dataオプションの直下プロパティは後から追加できないため、
-        //内容が決まってなくても空のデータとして定義しておく(なるべく後から代入されるデータと同じ型で定義する)
-        //例
-        newTodoText: '',
-        visitcount: 0,
-        hideCompletedToDos: false,
-        todos: [],
-        error: null
+        //オブジェクトデータ
+        message: {
+            value: 'hello Vue.js!'
+        },
+
+        //配列データ
+        list: ['りんご', 'バナナ', 'いちご'],
+        //別のデータを使用してlistから取り出す要素を動的に
+        num:1
     }
   })
-
-state.count++ // state.countはリアクティブデータになっている
