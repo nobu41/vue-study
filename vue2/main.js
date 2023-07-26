7aeb7c517aedf9a5f16019ea2fa3de989334beb7
user <>
2023-07-26 09:19:58 +0900

    【vue2】thisの参照について

--- vue2/main.js
+++ vue2/main.js
@@ -4,10 +4,27 @@ var app = new Vue({
         count: 0,
     },
     methods: {
-        //ボタンクリック時のハンドラ
+        //Vueインスタンスのthisを使用したいときは関数の記述方法に気をつける
         increment:function(){
-            //methods内でプロパティ（count）を使用する場合はthisが必要
-            this.count += 1 //処理は再代入するだけでok
+            //あらかじめ別の変数に代入しておけばsetTimeout内で使用できる
+            var  vm = this
+            setTimeout(function() { vm.count++ }, 100)
+            //bindでthisを紐付け関数を作成する
+            setTimeout(functon() { this.count++ }.bind(this),100)
+            //アロー関数はsetTimeoutが呼ばれるincrementメソッドのthis
+            //つまりvueインスタンスにアクセスできる
+            setTimeout(() >= { this.count++ }, 100)
+            //無名関数ではなくインスタンスのメソッドを指定する
+            setTimeout(this.callback, 100)
+
+            //別パターン
+            //thisを用いてvueインスタンスに
+            //アクセスする必要があるメソッドの定義にはアロー関数は使えない
+            //
+            increment:() {
+                //これはvueインスタンスのthisを使用できる
+            }
+
         }
     }
 })
