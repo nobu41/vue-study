3a74216d64e1ba270132b17def84a25e26816dc0
n-ebine <n-ebine@visual-w.co.jp>
2023-08-07 10:31:22 +0900

    【vue2】複数の属性のデータバインディング

--- vue2/main.js
+++ vue2/main.js
@@ -1,17 +1,12 @@
 var app = new Vue({
     el: '#app',
     data: {
-        isChild: true,
-        isActive: false,
-        textColor: 'red',
-        bgColor:'lightgray',
-        classObject: {
-            child: true,
-            'is-active': false, //class名に‐を使う場合は'で囲む
-        },
-        styleObject: {
-            color: 'blue',
-            backgroundColor: 'lightgray' //css名に-がある場合は大文字でつなぐ（JSと同じ）
-        },
+        item: {
+            id: 1,
+            src: 'item.jpg',
+            alt: '商品サムネ1',
+            width: 100,
+            height: 100
+        }
     }
 })
