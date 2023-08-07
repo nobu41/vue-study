cbb0e594ee6d78c62bdbfaa8add1e1b7b84329f3
n-ebine <n-ebine@visual-w.co.jp>
2023-08-07 10:13:20 +0900

    【vue2】クラスとスタイルのデータバインディング2

--- vue2/main.js
+++ vue2/main.js
@@ -4,6 +4,14 @@ var app = new Vue({
         isChild: true,
         isActive: false,
         textColor: 'red',
-        bgColor:'lightgray'
+        bgColor:'lightgray',
+        classObject: {
+            child: true,
+            'is-active': false, //class名に‐を使う場合は'で囲む
+        },
+        styleObject: {
+            color: 'blue',
+            backgroundColor: 'lightgray' //css名に-がある場合は大文字でつなぐ（JSと同じ）
+        },
     }
 })
