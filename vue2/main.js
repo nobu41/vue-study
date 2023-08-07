788ba923be6a5ffbd15b9d8277b29b298c24ea02
n-ebine <n-ebine@visual-w.co.jp>
2023-08-07 10:42:49 +0900

    【vue2】SVGのデータバインディング

--- vue2/main.js
+++ vue2/main.js
@@ -1,12 +1,6 @@
 var app = new Vue({
     el: '#app',
     data: {
-        item: {
-            id: 1,
-            src: 'item.jpg',
-            alt: '商品サムネ1',
-            width: 100,
-            height: 100
-        }
+        radius:50
     }
 })
