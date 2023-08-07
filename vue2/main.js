3525ffc72519054d4adecd7a2d2190d82f2d9357
user <>
2023-08-08 08:43:05 +0900

    【vue2】要素を繰り返し描画 + keyの役割

--- vue2/main.js
+++ vue2/main.js
@@ -1,7 +1,22 @@
 var app = new Vue({
     el: '#app',
     data: {
-        ok: true,
-        type: 'B',//　文字は'で囲むこと
+        list: [
+            {
+                id: 1,
+                name: 'スライム',
+                hp: 100
+            },
+            {
+                id: 2,
+                name: 'ドラゴン',
+                hp: 500
+            },
+            {
+                id: 3,
+                name: 'ゴブリン',
+                hp: 300
+            }
+        ],
     }
 })
