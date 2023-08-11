b2af0c9a096dca7294f081526b5af4774e481453
user <>
2023-08-11 16:34:45 +0900

    【vue2】外部からデータを取得する

--- vue2/main.js
+++ vue2/main.js
@@ -1,7 +1,19 @@
 var app = new Vue({
     el: '#app',
     data: {
-        text: 'veu',
-        list: ['スライム', 'ゴブリン', 'ドラゴン']
+        list: [],//リセットよして空にしておく
     },
+    //特定のタイミングで自動的に処理を挟むにはライフサイクルフックを使用する
+    //インスタンス作成後すぐにデータが欲しいのでcreatedを使う
+    created: function() {
+        //リストデータはリアクティブなデータのためレスポンスを受け取って
+        //データに代入した時点で画面のリストも更新される。
+        //しかし、レスポンスが遅いとその間何も表示されないためローディングアニメを入れると良い
+        axios.get('list.json').then(function(response){
+            //取得完了したらlistに代入
+            this.list = response.data
+        }.bind(this)).catch(function(e){
+            console.error(e)
+        })
+    }
 })
