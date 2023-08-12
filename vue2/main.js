bf1f60774b603abba9239ab5b4bfb2ead0e64f40
user <>
2023-08-12 09:28:04 +0900

    【vue2】$el や $refs は一時的な更新のため注意 （DOMを直接参照する $el と $refs）

--- vue2/main.js
+++ vue2/main.js
@@ -15,8 +15,25 @@
 //インスタンスのメソッド内から this.$refs.hello（任意の名前） でアクセスできる。
 //めっちゃ便利らしい・・・覚えること
 
+//【$el や $refs は一時的な更新のため注意】
+//$el や $refs 仮想DOM ではないため操作するたびに描画するためDOMの更新には向かない
+//以下のように$refs を使ってDOMを操作してもデータに変化があれば仮想DOMによって上書きされることがある
+//例：カウントアップをして増加させてもv-ifの切り替えを行うと値が初期の 0 に戻ってしまう。
+// リアルなDOMの変更は仮想DOMに対して影響を与えないため
+
 var app = new Vue({
     el: '#app',
+    data:{
+        show: true
+    },
+    methods:{
+        handleClick () {
+            var count = this.$refs.count
+            if (count) {
+                count.innerText = parseInt(count.innerText,10) + 1
+            }
+        }
+    },
     mounted: function() {
         console.log(this.$el)
         //以下がコンソールに表示される = ルートが表示されるね
