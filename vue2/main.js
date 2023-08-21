649ac18cbd1efd8020e730fe5292f9f5d85b21a1
user <>
2023-08-22 08:46:53 +0900

    【vue2】フォーム入力の取得

--- vue2/main.js
+++ vue2/main.js
@@ -2,10 +2,15 @@ var app = new Vue({
     el: '#app',
     data: {
         show:true,
+        message:'hello!'
     },
     methods: {
         handleClick: function () {
             alert('クリックしたよ')
-          } 
-      }    
+          },
+        handleInput: function (event){
+            //代入前に何か処理を行う
+            this.message =event.target.value
+        }
+    }
 })
