4f4c2eb0f74c86ca8a2faca97a9e183448908af4
n-ebine <n-ebine@visual-w.co.jp>
2023-09-01 16:43:15 +0900

    【veu3-vite】class 属性：配列（Binding）

--- study-vue3-vite/src/App.vue
+++ study-vue3-vite/src/App.vue
@@ -1,9 +1,7 @@
 <script setup>
-  const isActive = true;
-  const isActive2 = 'active2';
-  const isBlack = true;
+  const isActive = 'active';
+  const isBlack = 'back';
 
-  const link = 'https://google.com';
 </script>
 
 <template>
@@ -11,7 +9,6 @@
   <!--Binding(バインディング)
     html タグの属性の設定が可能（例：hrefなど）
     <a v-bind:href="変数名">リンク名</a> -->
-  <a v-bind:href="link">Google</a>
 
   <!--
   v-bind を頻繁に利用する例として以下がある。
@@ -22,49 +19,18 @@
   -->
 
 
-  <!-- class 属性 -->
-  <p class="active">v-bindの設定方法の確認</p>
+  <!-- 複数の class を適用したい場合には配列を利用 -->
+  <p :class="[isActive, isBlack]">v-bindの設定方法の確認</p>
 
+  <!-- 配列の中でオブジェクトを利用することもできる -->
+  <p :class="[{ active: 'isActive' }, isBlack]">v-bindの設定方法の確認</p>
 
-  <!-- オブジェクトのプロパティに class 名を設定し、
-    値に変数名または true, false を利用することで class を適用するかどうかを設定
-        v-bind:class="{class名: 変数名 or (true or false)}" -->
-  <p class="underLine" :class="{ active: isActive }">v-bindの設定方法の確認</p>
-
-
-  <!-- underLine と active クラスを isActive が true の場合に適用したい場合 -->
-  <p :class="{ 'underLine active': isActive }">v-bindの設定方法の確認</p>
-
-
-  <!-- class の中には複数の class を設定できる -->
-  <p class="underLine" :class="{ active: isActive, back: isBlack }">
-    v-bindの設定方法の確認
+  <!-- 配列の中で演算子を利用することもできる -->
+  <p :class="[isActive && 'active', isBlack]">v-bindの設定方法の確認</p>
+  
+  <p :class="[isActive ? 'active' : 'underLine', isBlack]">
+  v-bindの設定方法の確認
   </p>
-
-  <!-- v-bind による複数の class の適用 -->
-  <p :class="{ 'underLine active': isActive2, back: isBlack }"></p>
-
-
-  <!-- v-bind ディレクティブを設定した class の中では論理演算子(&&)を利用して設定できる
-    v-bind:class="{変数名　&& 'class名'}"
-    isActive が true の時に active クラスが適用
-   -->
-   <p :class="isActive && 'active'">v-bindの設定方法の確認</p>
-
-
-  <!-- 利用頻度は低いが論理演算子(||)も利用できる。
-    変数が false の時に class が適用されます。
-    v-bind:class="{変数名　|| 'class名'}"
-    -->
-
-  <!-- 三項演算子を利用できる。
-    isActive が true の場合は active, false の場合は underLine が適用 -->
-  <p :class="isActive ? 'active' : 'underLine'">v-bindの設定方法の確認</p>
-
-
-  <!-- 変数名を設定して変数名に設定している class を適用できる -->
-  <p :class="isActive2">v-bindの設定方法の確認</p>
-
 
 
 </template>
