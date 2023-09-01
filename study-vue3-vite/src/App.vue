05eeb04f454aad2fa04ff845d7a74c43b67da01d
n-ebine <n-ebine@visual-w.co.jp>
2023-09-01 14:31:09 +0900

    【vue3-vite】v-text、v-html（初めての Hello World）

--- study-vue3-vite/src/App.vue
+++ study-vue3-vite/src/App.vue
@@ -1,17 +1,35 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+// script タグ内の setup はscript タグの中で
+// Vue.js のコード(Composition API)の記述を楽にするために必要な設定
+// 削除しても JavaScript のコードが実行できなくなるわけではないが
+// Vue.js を利用したコードは動かない
+
+  console.log('Hello World');
+  const message = 'Hello World';
+  const message2 = '<h2>Hello World</h2>';
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>Vue 3 入門</h1>
+  <p>{{ message }}</p>
+
+  <!-- マスタッシュは変数を表示するだけではなく JavaScript のコードを実行できる -->
+  <div>{{ message.length * 10 }}</div>
+
+  <!-- マスタッシュの中では三項演算子を実行できる。message の文字列が 10 よりも長い場合は Long と表示、10 以下だと Short と表示 -->
+  <div>{{ message.length > 10 ? 'Long' : 'Short' }}</div>
+
+  <!-- v-text ディレクティブ
+  タグの中に文字を入力するとエラーになる  -->
+  <p v-text="message"></p>
+
+  <!-- v-html ディレクティブ
+  変数に html タグを含めるとそのタグをブラウザ上でタグとして認識させる  -->
+  <div v-html="message2"></div>
+
+
+
+
 </template>
 
 <style scoped>
