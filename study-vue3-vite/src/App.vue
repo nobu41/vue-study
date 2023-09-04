9f981a6d110dd9f131d3b71cf83e3eec3c1875e1
n-ebine <n-ebine@visual-w.co.jp>
2023-09-04 11:35:28 +0900

    【vue3-vite】v-if, v-show（条件分岐）

--- study-vue3-vite/src/App.vue
+++ study-vue3-vite/src/App.vue
@@ -1,60 +1,54 @@
 <script setup>
-  const activeColor = 'red';
-  const fontStress = '900';
+    const error = 'エラー発生';
+    const error2 =true;
+    const error3 ='';
+    const stock = 5;
 
-  const styleObjcet = {
-    color: 'red',
-    fontWeight: 900,
-  };
-
-  const styleObjcet2 = 'color:red;font-weight:900';
-
+    const error_vshow = true;
 </script>
 
 <template>
   <h1>Vue 3 入門</h1>
-  <!--Binding(バインディング)
-    html タグの属性の設定が可能（例：hrefなど）
-    <a v-bind:href="変数名">リンク名</a> -->
+<!-- ---------------------------------
+ 条件分岐
+--------------------------------- -->
+<!-- v-if による分岐 -->
+    <!--     
+    <div v-if="条件式">内容</div>
+    <div v-else-if="条件式">内容</div>
+    <div v-else>内容</div>
+    -->
+    <div v-if="error">{{ error }}</div>
+    <hr>
 
-  <!--
-  v-bind を頻繁に利用する例として以下がある。
-  動的に表示させる画像を変更したい場合、条件によってボタンを押させないといったことが
-  v-bind ディレクティブを利用することで実装できます。
-  ・img タグの src 属性
-  ・button タグの disabled 属性
-  -->
+    <div v-if="error2">エラー発生2</div>
+    <hr>
 
-  <!-- style 属性の設定 -->
-  <!-- 変数 activeColor, fontStress を定義して v-bind を設定した style 属性の中で利用 -->
-  <p :style="{ color: activeColor, fontWeight: fontStress }">
-    v-bindの設定方法の確認
-  </p>
+    <!-- error3 変数に何も入っていない場合にエラーはありませんと表示される -->
+    <div v-if="error3">{{ error3 }}</div>
+    <div v-else>エラー3はないです</div>
+    <hr>
 
-  <!-- 通常の style のプロパティを利用した場合はシングルクォーテーションを使う -->
-  <p :style="{ color: activeColor, 'font-weight': fontStress }">
-    v-bindの設定方法の確認
-  </p>
-
-  <!-- 変数側で style を設定もできる。変数側でスタイルを設定することで template タグがすっきりする -->
-  <p :style="styleObjcet">v-bindの設定方法の確認</p>
-
-  <!-- 通常の style 属性のように記述することもできます。 -->
-  <p :style="styleObjcet2">v-bindの設定方法の確認</p>
+    <div v-if="stock > 5">在庫数に余裕があります</div>
+    <div v-else-if = "stock === 0">売り切れです</div>
+    <div v-else>在庫が少ないです</div>
+    <hr>
 
 
+<!-- v-show による制御 -->
+    <!-- <div v-show="条件式">内容</div> -->
+    <div v-show="error_vshow">エラー発生</div>
+    <hr>
+    <div v-show="stock > 3">stockが3より大きいです</div>
 
+
+<!-- v-if, v-show の違い -->
+<!-- 
+    v-if：falseの場合DOM自体表示されない
+    v-show：falseの場合diplay:none で非表示になる（処理の負荷低い=ドロップダウンメニューにおすすめ）
+-->
 </template>
 
 <style>
-  .active {
-    color: red;
-    font-weight: 900;
-  }
-  .underLine {
-    text-decoration: underline;
-  }
-  .back {
-    background-color: black;
-  }
+/* スタイルを記述 */
 </style>
