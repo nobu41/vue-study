747f404a9f7829d0167884496cb88bb3c1871620
n-ebine <n-ebine@visual-w.co.jp>
2023-09-05 17:27:45 +0900

    【vue3-vite】Computed vs Function（Computed プロパティ）

--- study-vue3-vite/src/App.vue
+++ study-vue3-vite/src/App.vue
@@ -1,19 +1,11 @@
 <script setup>
-//computed 関数を import
-import { computed } from 'vue';
-  const users = [
-    { id: 1, name: 'John Doe', email: 'john@test.com', admin: true },
-    { id: 2, name: 'Jane Doe', email: 'jane@example.com', admin: false },
-    { id: 3, name: 'Kevin MacDonald', email: 'kevin@test.com', admin: false },
-  ];
-  // Computed プロパティを利用して admin trueユーザのみ表示
-  const adminUsers = computed(() =>
-    users.filter((user) => user.admin === true)
-  );
-  
-  //filter 関数内で条件式を変更するだけで admin ではないユーザを取得
-  const generalUsers = computed(() => users.filter((user) => user.admin === false))
-
+//関数を import
+import { reactive, computed } from 'vue';
+  const user = reactive({
+    firstName: 'John',
+    lastName: 'Doe',
+  });
+  const fullName = () => `${user.firstName} ${user.lastName}`;
 </script>
 
 <template>
@@ -23,10 +15,10 @@ Computed プロパティ
 定義済みの変数を利用して計算、加工を行うことで
 元のデータとは異なる形でユーザに表示することができる
 --------------------------------- -->
-<!-- Computed プロパティ adminUsers を利用して繰り返し処理 -->
-<div v-for="user in adminUsers" :key="user.id">
-  <div>{{ user.id }} {{ user.name }} {{ user.email }}</div>
-</div>
+<!-- Computed vs Function -->
+<!-- Computed プロパティで記述した内容を関数で記述
+     マスタッシュの中で関数を利用する場合関数の名前の後ろに()が必要 -->
+<h2>fullName: {{ fullName() }}</h2>
 
 </template>
 
