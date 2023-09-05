37231735a72fe42e6aa086042a201ceabd5280ac
n-ebine <n-ebine@visual-w.co.jp>
2023-09-05 17:58:34 +0900

    【vue3-vite】Computed プロパティのキャッシュ機能の確認　Computed vs Function（Computed プロパティ）

--- study-vue3-vite/src/App.vue
+++ study-vue3-vite/src/App.vue
@@ -1,16 +1,21 @@
 <script setup>
 //関数を import
-import { reactive, computed } from 'vue';
-  const user = reactive({
-    firstName: 'John',
-    lastName: 'Doe',
-  });
-  //関数と Computed プロパティを同時に実行するために Computed プロパティの cFullName 関数を追加
-  const fullName = () => `${Math.random()} ${user.firstName} ${user.lastName}`;
-  const cFullName = computed(
-    () => `${Math.random()} $${user.firstName} ${user.lastName}`
-  );
+import { ref, reactive, computed } from 'vue';
+const count = ref(0);
 
+const user = reactive({
+  firstName: 'John',
+  lastName: 'Doe',
+});
+
+const fullName = () => {
+  console.log('Function');
+  return `${user.firstName} ${user.lastName}`;
+};
+const cFullName = computed(() => {
+  console.log('Computed Propety');
+  return `${user.firstName} ${user.lastName}`;
+});
 </script>
 
 <template>
@@ -21,8 +26,7 @@ Computed プロパティ
 元のデータとは異なる形でユーザに表示することができる
 --------------------------------- -->
 <!-- Computed vs Function -->
-<!-- Computed プロパティのキャッシュ機能について -->
-<!-- キャッシュ機能を確認するために tempalte タグの中で複数の関数と computed プロパティを実行 -->
+<!-- input 要素を利用して reactive な変数を更新 -->
 <h2>fullName: {{ fullName() }}</h2>
 <h2>fullName: {{ fullName() }}</h2>
 <h2>fullName: {{ fullName() }}</h2>
@@ -32,7 +36,7 @@ Computed プロパティ
 <!-- fullName 関数ではすべての fullName 関数の実行で Math.randam 関数が実行されるため異なる値が表示されていますが Computed プロパティの cFullName はキャッシュ機能を持っているため一度 Math.randam 関数が実行されるだけで後は同じ値を持 -->
 
 <input v-model="user.firstName" />
-
+<button @click="count++">Count:{{ count }}</button>
 </template>
 
 <style>
