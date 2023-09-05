6e8fe9d7f8c4b17e04fc1d2791b770508378aeb0
n-ebine <n-ebine@visual-w.co.jp>
2023-09-05 16:18:11 +0900

    【vue3-vite】input 要素　ref 関数（入力フォーム）

--- study-vue3-vite/src/App.vue
+++ study-vue3-vite/src/App.vue
@@ -1,34 +1,28 @@
 <script setup>
-// reactive の設定
-// vue から ref 関数を import 
-import { ref } from 'vue';
+  import { ref } from 'vue';
+  const message = ref('Hello World');
 
-// script タグ内で count の数を増やす場合
-const state = ref({
-  count: 0,
-});
-const addCount = () => {
-  state.value.count++;
-};
+  const clickButton = () => {
+    console.log(message.value);
+  };
 </script>
 
 <template>
   <h1>Vue 3 入門</h1>
 <!-- ---------------------------------
- Reactivity
+ 入力フォーム
 --------------------------------- -->
-<!-- reactive の設定 -->
-<!-- count の値を表示する場合はオブジェクトだから state.count　値を増やす場合も state.count++ -->
-<!-- script タグ内で count の数を増やす場合 -->
-<button type="button" @click="addCount">count is: {{ state.count }}</button>
+<!-- input 要素 -->
 
-<!-- ref と reactive の違い -->
-<!-- ref 関数と reactive 関数の一番の違い
-ref は script タグ内で値にアクセスする場合に value を利用する
-reactive ではオブジェクトのみを扱うのに対して
-ref では primitive な値(string や boolen など)もオブジェクトも設定することができる -->
+<!-- ref 関数で定義した場合 -->
+<p>{{ message }}</p>
+<input v-model="message" />
+<div><button @click="clickButton">Click</button></div>
 
-
+<!-- v-modelは以下のように
+     value に v-bind を設定し input イベントを利用して文字を入力する度に
+     input 要素に入力した値を$event オブジェクトから取得して設定している
+<input :value="message" @input="message = $event.target.value" /> -->
 
 
 </template>
