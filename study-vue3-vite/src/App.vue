6c34f1946e8aa212077a42445e28242abdd1e495
n-ebine <n-ebine@visual-w.co.jp>
2023-09-05 17:10:52 +0900

    【vue3-vite】リストと分岐（Computed プロパティ）

--- study-vue3-vite/src/App.vue
+++ study-vue3-vite/src/App.vue
@@ -1,13 +1,19 @@
 <script setup>
-//reactive, computed 関数を import
-  import { reactive, computed } from 'vue';
-  const user = reactive({
-    firstName: 'John',
-    lastName: 'Doe',
-  });
+//computed 関数を import
+import { computed } from 'vue';
+  const users = [
+    { id: 1, name: 'John Doe', email: 'john@test.com', admin: true },
+    { id: 2, name: 'Jane Doe', email: 'jane@example.com', admin: false },
+    { id: 3, name: 'Kevin MacDonald', email: 'kevin@test.com', admin: false },
+  ];
+  // Computed プロパティを利用して admin trueユーザのみ表示
+  const adminUsers = computed(() =>
+    users.filter((user) => user.admin === true)
+  );
+  
+  //filter 関数内で条件式を変更するだけで admin ではないユーザを取得
+  const generalUsers = computed(() => users.filter((user) => user.admin === false))
 
-  //関数の戻り値が template タグの中で設定した fullName に表示
-  const fullName = computed(() => `${user.firstName} ${user.lastName}`);
 </script>
 
 <template>
@@ -17,8 +23,10 @@ Computed プロパティ
 定義済みの変数を利用して計算、加工を行うことで
 元のデータとは異なる形でユーザに表示することができる
 --------------------------------- -->
-<!-- <h2>fullName: {{ user.firstName }} {{ user.lastName }}</h2> -->
-<h2>fullName: {{ fullName }}</h2>
+<!-- Computed プロパティ adminUsers を利用して繰り返し処理 -->
+<div v-for="user in adminUsers" :key="user.id">
+  <div>{{ user.id }} {{ user.name }} {{ user.email }}</div>
+</div>
 
 </template>
 
