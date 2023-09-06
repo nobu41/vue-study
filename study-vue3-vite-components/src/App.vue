fb88c44e11b7155530bee3434916e56f8cdf794a
n-ebine <n-ebine@visual-w.co.jp>
2023-09-06 13:24:54 +0900

    【vue3-vite-components】Hello コンポーネントの作成、コンポーネントの親子関係、コンポーネントの再利用（初めてのコンポーネント）

---
+++ study-vue3-vite-components/src/App.vue
@@ -0,0 +1,40 @@
+<script setup>
+// コンポーネントファイルを作成
+// 別のコンポーネントファイルから作成したコンポーネントファイルを import
+// template タグの中に import したコンポーネントのタグを埋め込む
+//import HelloWorld from './components/HelloWorld.vue'
+
+//import した Hello コンポーネントの内容が表示
+import Hello from './components/Hello.vue';
+</script>
+
+<template>
+  <h1>Vue 3 入門</h1>
+  <!-- Hello コンポーネント -->
+  <Hello />
+  
+  <!-- コンポーネントの親子関係 -->
+  <!-- import するコンポーネントと import されるコンポーネントで親子関係を持つことになる。
+       import を行うコンポーネントを親コンポーネントと呼び、
+       import されるコンポーネントを子コンポーネントと呼ぶ -->
+
+
+  <!-- コンポーネントの再利用 -->
+  <!-- import した Hello コンポーネントを template タグの中で複数回追加できる -->
+  <!-- 親コンポーネント側のファイルでは何度でもコンポーネントを利用することができる -->
+  <Hello />
+  <Hello />
+
+
+</template>
+
+<style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
