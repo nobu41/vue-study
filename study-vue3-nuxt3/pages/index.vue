88bdbe621dd81d0befa1134c9c11669796248f74
n-ebine <n-ebine@visual-w.co.jp>
2023-09-08 16:53:31 +0900

    【vue3-nuxt3】Components の Lazy Loading（components ディレクトリ）

--- study-vue3-nuxt3/pages/index.vue
+++ study-vue3-nuxt3/pages/index.vue
@@ -1,3 +1,17 @@
 <template>
-  <h1>Main Pageコンテンツ</h1>
+  <div>
+    <h1>Main Page</h1>
+    <button @click="handleClick">Coupon Get</button>
+    
+    <!-- <Coupon v-if="show" />にLazy を追加する -->
+    <!-- ボタンクリック後の Coupon コンポーネントがダウンロード（表示）される -->
+    <LazyCoupon v-if="show" />
+  </div>
 </template>
+<script setup>
+  const show = ref(false);
+
+  const handleClick = () => {
+    show.value = true;
+  };
+</script>
