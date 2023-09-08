730e59146e8aa567c417f9e27ec7671584e3cef1
n-ebine <n-ebine@visual-w.co.jp>
2023-09-08 16:35:14 +0900

    【vue3-nuxt3】Auto Imports　Component Name（components ディレクトリ）

--- study-vue3-nuxt3/layouts/default.vue
+++ study-vue3-nuxt3/layouts/default.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
-    <Navbar />
+    <!-- Auto Imports -->
+    <!-- ネストされているコンポーネントファイルは
+         ディレクトリとファイル名でインポートできる
+         以下は
+         components/nav/Navbar.vue = NavNavbar  -->
+    <NavNavbar />
+
+    <!-- nav/NavBar.vue の場合はNavNavBar としてもだめ
+         NavNavBar では、nav/nav/Bar.vue ファイルを
+         import していることになる -->
+
     <slot />
   </div>
 </template>
--- study-vue3-nuxt3/nuxt.config.ts
+++ study-vue3-nuxt3/nuxt.config.ts
@@ -1,12 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  components: [
-    {
-      path: '~/components',
-      //pathPrefix の値が false の場合Navbar ファイルが一つであれば
-      //nav ディレクトリに保存されていても Navbar タグで Auto imports される
-      pathPrefix: false,
-    },
-  ],
+  // components: [
+  //   {
+  //     path: '~/components',
+  //     //pathPrefix の値が false の場合Navbar ファイルが一つであれば
+  //     //nav ディレクトリに保存されていても Navbar タグで Auto imports される
+  //     pathPrefix: false,
+  //   },
+  // ],
 })
