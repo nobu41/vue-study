9fe25de0bd11067339b073c840520abaf1daaad8
n-ebine <n-ebine@visual-w.co.jp>
2023-09-08 15:09:54 +0900

    【vue3-nuxt3】slot の設定（ディレクトリ構成）

--- study-vue3-nuxt3/pages/index.vue
+++ study-vue3-nuxt3/pages/index.vue
@@ -1,30 +1,3 @@
 <template>
-  <div>
-    <button @click="enableCustomLayout">Update layout</button>
-  </div>
+  <h1>Main Page</h1>
 </template>
-<script setup>
-//ページに適用するレイアウトを動的に変更
-  //"Update layout"ボタンをクリックすると custom レイアウトが適用
-  function enableCustomLayout() {
-    //レイアウトの適用に setPageLayout 関数を利用
-    setPageLayout('custom');
-  }
-  //layout を false に設定=デフォルトのレイアウトが設定
-  definePageMeta({
-    layout: false,
-  });
-
-//useRoute 関数を利用して route.meta.layout の値を
-//設定することでも動的に変更することは可能
-// const route = useRoute();
-// const enableCustomLayout = () => {
-//   route.meta.layout = 'custom';
-// };
-// definePageMeta({
-//   layout: false,
-// });
-
-
-
-</script>
