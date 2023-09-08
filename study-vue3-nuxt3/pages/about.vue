fe1d1c3a5dca6d34d04b20fd6bde7bc33f632529
n-ebine <n-ebine@visual-w.co.jp>
2023-09-08 11:55:32 +0900

    【vue3-nuxt3】NuxtLayout コンポーネント layouts（ディレクトリ構成）

--- study-vue3-nuxt3/pages/about.vue
+++ study-vue3-nuxt3/pages/about.vue
@@ -1,4 +1,18 @@
 <template>
-  <h3>About Page(pages/about.vue)</h3>
-  <p>layouts/default.vue適用</p>
+
+<!-- レイアウトファイルは pages/about.vue ファイルに直接
+     NuxtLayout コンポーネントを利用することもできる -->
+<!-- name props にレイアウトファイル名を設定もできる -->
+
+<!-- page ファイルで NuxtLayout を利用する場合は
+     NuxtLayout タグがルートエレメントにならないようにする以下はルートエレメントは div -->
+    <div>
+      <NuxtLayout style="test">
+        <h1>About Page</h1>
+        <h3>About Page(pages/about.vue)</h3>
+        <p>layouts/default.vue適用</p>
+      </NuxtLayout>
+    </div>
+
+
 </template>
