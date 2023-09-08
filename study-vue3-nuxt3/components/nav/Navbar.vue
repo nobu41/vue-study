c6cf7810fe02d3b44127b6265ee726e5b12abe79
n-ebine <n-ebine@visual-w.co.jp>
2023-09-08 17:06:10 +0900

    【vue3-nuxt3】NuxtLink（ナビゲーション設定）

--- study-vue3-nuxt3/components/nav/Navbar.vue
+++ study-vue3-nuxt3/components/nav/Navbar.vue
@@ -1,6 +1,19 @@
 <template>
   <nav>
+<!--     
     <a href="/">Home2</a>
     <a href="/about">About2</a>
+ -->
+<!-- NuxtLink：ページの中で更新が必要な箇所だけ更新が行われる -->
+    <NuxtLink to="/">Home</NuxtLink>
+    <NuxtLink to="/about">About</NuxtLink>
+
+<!-- 外部のサイトの場合は通常の a タグのように動作target="_blank"も使える  -->
+    <NuxtLink href="https://google.com" target="_blank">
+      Google
+    </NuxtLink>
+
+<!-- hrefも使える -->
+    <NuxtLink href="/about">test</NuxtLink>
   </nav>
 </template>
