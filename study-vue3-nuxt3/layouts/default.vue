0654b4842be574c105f65c7d62109890fbf81b80
n-ebine <n-ebine@visual-w.co.jp>
2023-09-08 15:38:25 +0900

    【vue3-nuxt3】名前付き slot の設定（ディレクトリ構成）

--- study-vue3-nuxt3/layouts/default.vue
+++ study-vue3-nuxt3/layouts/default.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <nav>ナビゲーションバーなど(layouts/default.vue)</nav>
+    <!-- 名前付き slot を追加。
+         slot タグに name props を任意の名前で追加 -->
+    <slot name="header" />
+    <nav>こにナビゲーションバーを入れる</nav>
     <slot />
   </div>
 </template>
