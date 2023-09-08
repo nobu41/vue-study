e61e190bc23a3a6ed378db53bd7664675f58c7a3
n-ebine <n-ebine@visual-w.co.jp>
2023-09-08 15:43:44 +0900

    【vue3-nuxt3】（components ディレクトリ）

--- study-vue3-nuxt3/layouts/default.vue
+++ study-vue3-nuxt3/layouts/default.vue
@@ -2,8 +2,7 @@
   <div>
     <!-- 名前付き slot を追加。
          slot タグに name props を任意の名前で追加 -->
-    <slot name="header" />
-    <nav>こにナビゲーションバーを入れる</nav>
+    <Navbar />
     <slot />
   </div>
 </template>
--- study-vue3-nuxt3/pages/index.vue
+++ study-vue3-nuxt3/pages/index.vue
@@ -1,23 +1,3 @@
 <template>
-  <div>
-    <!-- NuxtLayoutのname に default を設定 -->
-    <NuxtLayout name="default">
-      <!-- template タグに#header を設定　
-           名前付き slot を設定した場所に
-           表示させたい内容を記述 -->
-      <template #header>ヘッダー</template>
-      <h1>Main Page</h1>
-    </NuxtLayout>
-
-<!-- NuxtLayout に明示的に name props で default を設定しない場合は
-     definePageMeta 関数により default のレイアウトが適用されないため
-     name の設定が必須。NuxtLayout タグがない場合は、エラーになる -->
-
-  </div>
+  <h1>Main Page</h1>
 </template>
-
-<script setup>
-  definePageMeta({
-    layout: false,
-  });
-</script>
