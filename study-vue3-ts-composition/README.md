0e23d7fef9d79346e962dee91e9ee8b9814a4239
n-ebine <n-ebine@visual-w.co.jp>
2023-11-22 11:42:46 +0900

    【vue3×ts×compositon】導入編：ViteとVueプロジェクト、Vueプログラミングの基本

---
+++ study-vue3-ts-composition/README.md
@@ -0,0 +1,10 @@
+# 参考書：Vue3フロントエンド開発の教科書
+
+## 導入編：ViteとVueプロジェクト（chap02）
+### Vueプロジェクトの作成と実行（hello-vue）
+
+## 導入編：Vueプログラミングの基本（chap03）
+### Vueのコンポーネントと基本構文（mustache）
+### リアクティブシステム（reactive-system）
+### リアクティブデータ用意のバリエーション（computed）
+### refのオブジェクトをまとめてリアクティブにする（reactive-function）
---
+++ study-vue3-ts-composition/chap03/computed/src/App.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import {ref, computed} from "vue";
+
+// 半径の初期値を乱数で取得。
+const radiusInit = Math.round(Math.random() * 10);
+// 円周率のテンプレート変数を用意。
+const PI = ref(3.14);
+// 半径のテンプレート変数を用意。
+const radius = ref(radiusInit);
+// 円の面積の算出プロパティを用意。
+const area = computed(
+	(): number => {
+		return radius.value * radius.value * PI.value;
+	}
+);
+//アロー関数の省略系（可読性が悪いため非推奨）
+// const area = computed(
+// 	(): number => radius.value * radius.value * PI.value
+// );
+
+//無名関数ver.（thisの扱いが変化するため非推奨）
+// const area = computed(
+// 	function(): number {
+// 		return radius.value * radius.value * PI.value;
+// 	}
+// );
+// 半径のテンプレート変数に新しい乱数を1秒ごとに格納。
+setInterval(
+	():void => {
+		radius.value = Math.round(Math.random() * 10);
+	},
+	1000
+);
+// area.value = 300; ← computedは読み取り専用。値を代入しようとするとエラーになる
+</script>
+
+<template>
+	<p>半径{{radius}}の円の面積を円周率{{PI}}で計算すると、{{area}}</p>
+</template>
